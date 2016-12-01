48b60e18b122ea6e3010bc0aecccbeace04f27c3
Felipe <fozzimus@gmail.com>
2016-12-01 11:56:38 -0200

    Temporary Icons added and files created
    armas.csv and unidades.csv contain the infor for the cards
    
    attack.svg and defend.svg are temporary icons for the cards
    
    deck.rb and layout.rb have been initially customized.
    background color was also changed to army green.

--- specops/deck.rb
+++ specops/deck.rb
@@ -1,6 +1,23 @@
 require 'squib'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
-  save format: :png
+Squib::Deck.new(cards: 7, layout: 'layout.yml') do
+  background color: '#4b5320'
+  deck = csv file: 'unidades.csv'
+
+  text str: deck['Nome'], layout: :Title
+
+  svg file: 'attack.svg', layout: :AttackIcon
+  text str: deck['Attack'], layout: :AttackText
+
+  svg file: 'defend.svg', layout: :DefendIcon
+  text str: deck['Defend'], layout: :DefendText
+
+  save_png prefix: 'bigger_'
 end
+
+#require 'squib'
+
+#Squib::Deck.new(cards: 3, layout: 'layout.yml') do
+#  text str: 'Hello, World!'
+#  save format: :png
+#end
